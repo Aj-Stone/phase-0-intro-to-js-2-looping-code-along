bd9edc1aafb04693d2565833761687f6457f4f4a
Alex Atsitty <atsitty.alex1128@gmail.com>
2022-01-31 11:48:18 -0700

    Done with assignment

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solutions in this file
+const people = ["Charlie", "Samip", "Ali"];
+function writeCards(people,event){
+    let newArr=[];
+    for( let i=0; i < people.length; i++){
+    let message = (`Thank you, ${people[i]}, for the wonderful ${event} gift!`)
+    newArr.push(message);
+    }
+    return newArr
+    }
+    writeCards(people,"surprise")
+
+    function countDown(a){
+    while (a>-1){
+        console.log(a--)
+    }
+}
+    countDown(10)
